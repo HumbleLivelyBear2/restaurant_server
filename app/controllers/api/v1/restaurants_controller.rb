94d2f13c71afd17f01a37bc8f6295b85249d323b
HumbleLivelyBear2 <jackmodo2012@gmail.com>
2013-05-20 15:47:20 +0800

    fix restaurants controller

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -5,28 +5,17 @@ class Api::V1::RestaurantsController < ApplicationController
       c_id = params[:category_id];
       a_id = params[:area_id];
 
-      begin
-      	puts "c_id =" +c_id.to_s
-      rescue Exception => e
-      	puts "c_id =" +0.to_s
+      if (a_id != nil && c_id != nil) 
+            c = Category.find(c_id)
+            rs = c.restaurants.where(:area_id => a_id)
+      elsif a_id != nil 
+            rs =  Restaurant.where(:area_id => a_id)
+      elsif c_id != nil
+            c = Category.find(c_id)
+            rs = c.restaurants
       end
-      
-      
-      begin
-      	puts "a_id =" +a_id.to_s
-      rescue Exception => e
-      	puts "a_id =" +0.to_s
-      end
-
-      c = Category.find(c_id)
-      if (a_id == nil)
-      	rs = c.restaurants
-      else
-      	rs = c.restaurants.where( :area_id => a_id)
-      end
-
-      # c = Category.find(c_id )
-      # rs = c.restaurants
+                        
       render :json => rs
+
 	end
 end

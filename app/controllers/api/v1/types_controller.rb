2294f7e4a430c80af23d3556bd6c172d99dbcbd1
Steven Ko <chunyuko85@gmaill.com>
2013-05-20 17:39:06 +0800

    area type ship

--- app/controllers/api/v1/types_controller.rb
+++ app/controllers/api/v1/types_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::TypesController < ApplicationController
     types = Type.select('id,name').all
     render :json => types
   end
+
+  def type_area_ship
+    ships = RestaurantTypeShip.joins(:restaurant).select("restaurants.area_id,type_id")
+    render :json => ships
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ RestaurantServer::Application.routes.draw do
     namespace :v1 do
       # resources :categories, :except => [:index]
       resources :categories, :only => [:index]
-      resources :types, :only => [:index]
+      resources :types, :only => [:index] do
+        collection do
+          get 'type_area_ship'
+        end
+      end
       resources :areas, :only => [:index]
       resources :area_categoryship, :only => [:index]
       resources :restaurants, :only => [:index, :show]

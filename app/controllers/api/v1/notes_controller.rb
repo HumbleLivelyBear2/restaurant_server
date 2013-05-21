a56a76257066f5988db60f2eeab4699fb2aab489
HumbleLivelyBear2 <jackmodo2012@gmail.com>
2013-05-22 00:09:42 +0800

    add note and restaurant route

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,8 +1,36 @@
 class Api::V1::NotesController < ApplicationController
 	def index
-		a_id = params[:area_id]
-		notes = Note.where(:area_id => a_id).index_select.paginate(:page => params[:page], :per_page => 15)
+		c_id = params[:category_id];
+        a_id = params[:area_id];
+        t_id = params[:type_id];
+
+        if (t_id ==nil)
+        	if (a_id != nil && c_id != nil) 
+                c = Category.find(c_id)
+                ids_rs = c.restaurants.where(:area_id => a_id).select("restaurants.id")
+                notes = Note.where(restaurant_id: ids_rs).index_select.paginate(:page => params[:page], :per_page => 15)
+            elsif (a_id != nil && c_id == nil) 
+                notes = Note.where(:area_id => a_id).index_select.paginate(:page => params[:page], :per_page => 15)
+            elsif (c_id != nil && a_id == nil)
+                c = Category.find(c_id)
+                ids_rs = c.restaurants.select("restaurants.id")
+                notes = Note.where(restaurant_id: ids_rs).index_select.paginate(:page => params[:page], :per_page => 15)
+            end 
+        elsif t_id !=nil
+            if (a_id != nil)
+                ids_rs = Restaurant.joins(:restaurant_type_ships).where("area_id = #{a_id} and type_id = #{t_id}").select("restaurants.id")               
+                notes = Note.where(restaurant_id: ids_rs).index_select.paginate(:page => params[:page], :per_page => 15)
+            elsif a_id == nil
+                ids_rs = Restaurant.joins(:restaurant_type_ships).where("type_id = #{t_id}").select("restaurants.id")
+                notes = Note.where(restaurant_id: ids_rs).index_select.paginate(:page => params[:page], :per_page => 15)
+            end
+        end  
+            	
+		
+
 		render :json => notes
+
+
 	end
 
 	def show
--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -4,16 +4,25 @@ class Api::V1::RestaurantsController < ApplicationController
 
             c_id = params[:category_id];
             a_id = params[:area_id];
+            t_id = params[:type_id];
 
-            if (a_id != nil && c_id != nil) 
-                  c = Category.find(c_id)
-                  rs = c.restaurants.where(:area_id => a_id).index_select.paginate(:page => params[:page], :per_page => 15)
-            elsif (a_id != nil && c_id == nil) 
-                  rs =  Restaurant.where(:area_id => a_id).index_select.paginate(:page => params[:page], :per_page => 15)
-            elsif (c_id != nil && a_id == nil)
-                  c = Category.find(c_id)
-                  rs = c.restaurants.index_select.paginate(:page => params[:page], :per_page => 15)
-            end
+            if (t_id == nil)
+                  if (a_id != nil && c_id != nil) 
+                        c = Category.find(c_id)
+                        rs = c.restaurants.where(:area_id => a_id).index_select.paginate(:page => params[:page], :per_page => 15)
+                  elsif (a_id != nil && c_id == nil) 
+                        rs =  Restaurant.where(:area_id => a_id).index_select.paginate(:page => params[:page], :per_page => 15)
+                  elsif (c_id != nil && a_id == nil)
+                        c = Category.find(c_id)
+                        rs = c.restaurants.index_select.paginate(:page => params[:page], :per_page => 15)
+                  end
+            elsif t_id !=nil
+                  if (a_id != nil)
+                        rs = Restaurant.joins(:restaurant_type_ships).where("area_id = #{a_id} and type_id = #{t_id}").index_select.paginate(:page => params[:page], :per_page => 15)
+                  elsif a_id == nil
+                        rs = Restaurant.joins(:restaurant_type_ships).where("type_id = #{t_id}").index_select.paginate(:page => params[:page], :per_page => 15)
+                  end
+            end                     
 
             render :json => rs
 

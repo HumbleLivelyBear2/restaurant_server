1335f94e27a5f4798fccb416d1f0eb9cd55eb6de
HumbleLivelyBear2 <jackmodo2012@gmail.com>
2013-05-22 18:17:55 +0800

    return x and y

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -8,21 +8,21 @@ class Api::V1::NotesController < ApplicationController
         	if (a_id != nil && c_id != nil) 
                 c = Category.find(c_id)
                 ids_rs = c.restaurants.where(:area_id => a_id).select("restaurants.id")
-                notes = Note.where(restaurant_id: ids_rs).index_select.paginate(:page => params[:page], :per_page => 15)
+                notes = Note.joins(:restaurant).where(restaurant_id: ids_rs).select("notes.id,notes.restaurant_id,notes.title, notes.intro, notes.pic_url, notes.link, restaurants.x_lan, restaurants.y_long").paginate(:page => params[:page], :per_page => 15)
             elsif (a_id != nil && c_id == nil) 
-                notes = Note.where(:area_id => a_id).index_select.paginate(:page => params[:page], :per_page => 15)
+                notes = Note.joins(:restaurant).where(:area_id => a_id).select("notes.id,notes.restaurant_id,notes.title, notes.intro, notes.pic_url, notes.link, restaurants.x_lan, restaurants.y_long").paginate(:page => params[:page], :per_page => 15)
             elsif (c_id != nil && a_id == nil)
                 c = Category.find(c_id)
                 ids_rs = c.restaurants.select("restaurants.id")
-                notes = Note.where(restaurant_id: ids_rs).index_select.paginate(:page => params[:page], :per_page => 15)
+                notes = Note.joins(:restaurant).where(restaurant_id: ids_rs).select("notes.id,notes.restaurant_id,notes.title, notes.intro, notes.pic_url, notes.link, restaurants.x_lan, restaurants.y_long").paginate(:page => params[:page], :per_page => 15)
             end 
         elsif t_id !=nil
             if (a_id != nil)
                 ids_rs = Restaurant.joins(:restaurant_type_ships).where("area_id = #{a_id} and type_id = #{t_id}").select("restaurants.id")               
-                notes = Note.where(restaurant_id: ids_rs).index_select.paginate(:page => params[:page], :per_page => 15)
+                notes = Note.joins(:restaurant).where(restaurant_id: ids_rs).select("notes.id,notes.restaurant_id,notes.title, notes.intro, notes.pic_url, notes.link, restaurants.x_lan, restaurants.y_long").paginate(:page => params[:page], :per_page => 15)
             elsif a_id == nil
                 ids_rs = Restaurant.joins(:restaurant_type_ships).where("type_id = #{t_id}").select("restaurants.id")
-                notes = Note.where(restaurant_id: ids_rs).index_select.paginate(:page => params[:page], :per_page => 15)
+                notes = Note.joins(:restaurant).where(restaurant_id: ids_rs).select("notes.id,notes.restaurant_id,notes.title, notes.intro, notes.pic_url, notes.link, restaurants.x_lan, restaurants.y_long").paginate(:page => params[:page], :per_page => 15)
             end
         end  
             	
